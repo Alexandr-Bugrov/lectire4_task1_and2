9c19a2d3d12ada8fe8a3c5fe1851030b5352fdea
Александр Бугров <bugor9682@gmail.com>
2022-06-05 13:15:24 +0300

    Исправлен 5 запрос

--- table_craete.sql
+++ table_craete.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS genre (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(40) NOT NULL UNIQUE
+	genre_name VARCHAR(40) NOT NULL UNIQUE
 );
 
 
 CREATE TABLE IF NOT EXISTS band (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(30) NOT NULL UNIQUE
+	band_name VARCHAR(30) NOT NULL UNIQUE
 );
 
 
 CREATE TABLE IF NOT EXISTS album (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(30) NOT NULL UNIQUE,
+	album_name VARCHAR(30) NOT NULL UNIQUE,
 	year_of_realise INTEGER NOT NULL
 );
 
@@ -27,28 +27,28 @@ CREATE TABLE IF NOT EXISTS track(
 
 CREATE TABLE IF NOT EXISTS collection(
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(30) NOT NULL UNIQUE,
+	collection_name VARCHAR(30) NOT NULL UNIQUE,
 	year_of_realise INTEGER NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS band_genre(
-	genre INTEGER REFERENCES genre(id),
-	band INTEGER REFERENCES band(id),
+	genre_id INTEGER REFERENCES genre(id),
+	band_id INTEGER REFERENCES band(id),
 	CONSTRAINT pk_band_genre PRIMARY KEY(genre, band)
 );
 
 
 CREATE TABLE IF NOT EXISTS band_album(
-	band INTEGER REFERENCES band(id),
-	album INTEGER REFERENCES album(id),
+	band_id INTEGER REFERENCES band(id),
+	album_id INTEGER REFERENCES album(id),
 	CONSTRAINT pk_band_album PRIMARY KEY(band, album)
 );
 
 
 CREATE TABLE IF NOT EXISTS tracks_in_collection(
-	track INTEGER REFERENCES track(id),
-	collection INTEGER REFERENCES collection(id),
+	track_id INTEGER REFERENCES track(id),
+	collection_id INTEGER REFERENCES collection(id),
 	CONSTRAINT pk_tracks_in_collection PRIMARY KEY(track, collection)
 );
 
--- task1.sql
+++ task1.sql
@@ -1,19 +1,19 @@
 SELECT name, year_of_realise, FROM album
     WHERE year_of_realise >= 2018;
 
-SELECT name, duration, FROM track
+SELECT track_name, duration, FROM track
     ORDER BY duration
     LIMIT 1;
 
-SELECT name FROM track
+SELECT track_name FROM track
     WHERE duration >= 210;
 
-SELECT name FROM collections
+SELECT collections_name FROM collections
     WHERE year_of_realise BETWEEN 2018 AND 2020;
 
-SELECT name FROM band
-    WHERE name NOT IN (' ');
+SELECT band_name FROM band
+    WHERE band_name NOT LIKE ('% %');
 
-SELECT name FROM track
-    WHERE name LIKE '%my%' OR name LIKE '%мой%';
+SELECT track_name FROM track
+    WHERE track_name LIKE '%my%' OR name LIKE '%мой%';
     
